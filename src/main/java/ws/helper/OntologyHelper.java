b6f6072febfba1ce717b668f2f7994d93b826294
Coppini <gcpavani@uolinc.com>
2019-11-07 20:01:45 -0300

    Faz inferencias entrarem no modelo geral

--- src/main/java/ws/helper/OntologyHelper.java
+++ src/main/java/ws/helper/OntologyHelper.java
@@ -1,16 +1,27 @@
 package ws.helper;
 
-import org.apache.jena.ontology.Individual;
-import org.apache.jena.ontology.OntModel;
-import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.*;
-import ws.WsApplication;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.List;
 import java.util.Objects;
+
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import ws.WsApplication;
 
 public class OntologyHelper {
 
@@ -19,15 +30,17 @@ public class OntologyHelper {
     private static final String schema = "http://www.semanticweb.org/hozer/ontologies/2019/9/untitled-ontology-2#";
     private static final File owl = new File(Objects.requireNonNull(WsApplication.class.getClassLoader().getResource("ws.ttl")).getFile());
     private static final Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
-    private static final String sparQLPrefixes = "PREFIX : <http://www.semanticweb.org/hozer/ontologies/2019/9/untitled-ontology-2#> \n" +
-            "PREFIX owl: <http://www.w3.org/2002/07/owl#> \n" +
-            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" +
-            "PREFIX xml: <http://www.w3.org/XML/1998/namespace> \n" +
-            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" +
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n\n";
+    private static final String sparQLPrefixes =
+            "PREFIX : <http://www.semanticweb.org/hozer/ontologies/2019/9/untitled-ontology-2#> \n" + "PREFIX owl: <http://www.w3" +
+                    ".org/2002/07/owl#> \n" + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" + "PREFIX xml: <http://www" +
+                    ".w3.org/XML/1998/namespace> \n" + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" + "PREFIX rdfs: <http://www" +
+                    ".w3.org/2000/01/rdf-schema#> \n\n";
+    private static final Reasoner r = ReasonerRegistry.getOWLReasoner();
+    private static final InfModel inf = ModelFactory.createInfModel(r, model);
 
     static {
         model.read(owl.getAbsolutePath(), TURTLE_LANGUAGE);
+        model.add(inf);
     }
 
     public static String sparql(String query) {
@@ -45,9 +58,12 @@ public class OntologyHelper {
         return ((OntModel) model).listIndividuals(clazz).toList();
     }
 
-    public static List<Individual> getAllIndividualsOfTypeFromModel(String type, Model specificModel) {
+    public static List<Individual> getAllIndividualsOfType(String type, Model specificModel) {
         Resource clazz = specificModel.getResource(schema + type);
         return ((OntModel) specificModel).listIndividuals(clazz).toList();
+    }
+    public static Model model() {
+        return model;
     }
 
     public static Property getProperty(String property) {
@@ -72,7 +88,8 @@ public class OntologyHelper {
 
         return res.getResourceModel();
     }
-//
+
+    //
     private static ResultSet getResultSetFromQuery(String query) {
         String fullQuery = sparQLPrefixes + query;
         Query q = QueryFactory.create(fullQuery);
--- src/main/java/ws/service/NavigationService.java
+++ src/main/java/ws/service/NavigationService.java
@@ -1,88 +1,60 @@
 package ws.service;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
-import org.apache.jena.assembler.Mode;
-import org.apache.jena.assembler.assemblers.ReasonerFactoryAssembler;
-import org.apache.jena.ontology.Individual;
-import org.apache.jena.ontology.OntModel;
-import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.rdf.model.*;
-import org.apache.jena.rdf.model.impl.PropertyImpl;
-import org.apache.jena.reasoner.Reasoner;
-import org.apache.jena.reasoner.ReasonerFactory;
-import org.apache.jena.reasoner.ReasonerRegistry;
-import org.apache.jena.reasoner.rulesys.RDFSRuleReasonerFactory;
-import org.apache.jena.util.FileManager;
-import org.apache.jena.vocabulary.ReasonerVocabulary;
-import org.springframework.stereotype.Service;
-import ws.WsApplication;
-import ws.helper.OntologyHelper;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.util.*;
+
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.springframework.stereotype.Service;
+import ws.helper.OntologyHelper;
 
 @Service
 public class NavigationService {
     private static final String TURTLE_LANGUAGE = "TTL";
 
-    private static final String schema = "http://www.semanticweb.org/hozer/ontologies/2019/9/untitled-ontology-2#";
-    private static final File owl = new File(Objects.requireNonNull(WsApplication.class.getClassLoader().getResource("ws.ttl")).getFile());
-    private static final Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RDFS_INF);
-    private static final String sparQLPrefixes = "PREFIX : <http://www.semanticweb.org/hozer/ontologies/2019/9/untitled-ontology-2#> \n" +
-            "PREFIX owl: <http://www.w3.org/2002/07/owl#> \n" +
-            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" +
-            "PREFIX xml: <http://www.w3.org/XML/1998/namespace> \n" +
-            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" +
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n\n";
-
-
-    List<Individual> ground;
-
     @PostConstruct
     private void postConstruct() {
-        FileManager.get().readModel(model, owl.getAbsolutePath());
-        Resource config = model.createResource();
-        config.addProperty(ReasonerVocabulary.PROPruleMode, "backward");
-        Reasoner r = RDFSRuleReasonerFactory.theInstance().create(config);
+        Model model = OntologyHelper.model();
+        String schema = OntologyHelper.defaultSchema();
 
-        OntModel inf = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF, model);
-
-        Property topOf = inf.getProperty(schema + "topOf");
-        Property bottomOf = inf.getProperty(schema + "bottomOf");
-        Property leftOf = inf.getProperty(schema + "leftOf");
-        Property rightOf = inf.getProperty(schema + "rightOf");
-        Property connects = inf.getProperty(schema + "connects");
+        Property topOf = model.getProperty(schema + "topOf");
+        Property bottomOf = model.getProperty(schema + "bottomOf");
+        Property leftOf = model.getProperty(schema + "leftOf");
+        Property rightOf = model.getProperty(schema + "rightOf");
+        Property connects = model.getProperty(schema + "connects");
 
         List<Property> sides = Arrays.asList(topOf, bottomOf, leftOf, rightOf, connects);
 
-        Individual commercialCenter = OntologyHelper.getAllIndividualsOfType("CommercialCenter").get(0);
+        Individual commercialCenter = OntologyHelper.getAllIndividualsOfType("CommercialCenter", model).get(0);
 
-        // Pra funfar, tem q mostrar rightOf Parede1-2 mas nao sei como fazer, talvez com InfModel
-//        sides.forEach(side -> {
-//            if (commercialCenter.hasProperty(side)) {
-//                Statement property = commercialCenter.getProperty(side);
-//                Resource otherSide = property.getResource();
-//                System.out.println("Is " + side.getLocalName() + otherSide.getLocalName());
-//            }
-//        });
-        checkAllConnectionsStartingFromResource(commercialCenter, sides);
+        checkAllConnectionsStartingFromResource(commercialCenter, sides, new HashSet<>());
 
 
         // montar a matrix vai ser pica, se eh q vai ser feito assim
     }
 
-    private void checkAllConnectionsStartingFromResource(Resource resource, List<Property> sides) {
-        List<Resource> resources = new ArrayList<>();
-        sides.forEach(side -> {
-            if (resource.hasProperty(side)) {
-                Statement property = resource.getProperty(side);
-                Resource otherSide = property.getResource();
-                resources.add(otherSide);
-                System.out.println(resource.getLocalName() +" Is " + side.getLocalName() + " " + otherSide.getLocalName());
-            }
-        });
-        resources.forEach(res -> checkAllConnectionsStartingFromResource(res, sides));
+    private void checkAllConnectionsStartingFromResource(Resource resource, List<Property> sides, Set<Resource> alreadyVisited) {
+        List<Resource> neighbours = new ArrayList<>();
+        if(!alreadyVisited.contains(resource)) {
+            alreadyVisited.add(resource);
+            sides.forEach(side -> {
+                if(resource.hasProperty(side)) {
+                    Statement property = resource.getProperty(side);
+                    Resource otherSide = property.getResource();
+                    neighbours.add(otherSide);
+                    System.out.println(resource.getLocalName() + " Is " + side.getLocalName() + " " + otherSide.getLocalName());
+                }
+            });
+        }
+
+        neighbours.forEach(res -> checkAllConnectionsStartingFromResource(res, sides, alreadyVisited));
     }
 
     // vai receber a URI de uma loja e vai achar o caminho mais curto ate qualquer chao daquela loja
