29c85675a6c6e91853f9990259745e84278cd560
Coppini <gcpavani@uolinc.com>
2019-11-16 13:27:19 -0300

    Coloca imagens

--- src/main/resources/static/js/loja-single.js
+++ src/main/resources/static/js/loja-single.js
@@ -22,15 +22,17 @@ function buildProductsList(storeLabel) {
 
     var arrayLength = products.length;
     for (var i = 0; i < arrayLength; i++) {
+        var imagePath = 'img/'+ products[i].name.split(" ").join("_") + '.jpg';
         var html = [
         '<div class="col-lg-4 col-md-6 mb-4">\n',
           '<div class="card h-100">\n',
-            '<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>\n',
+            '<a href="#"><img class="card-img-top" src="' + imagePath + '" height=253 width=144 alt=""></a>\n',
             '<div class="card-body">\n',
               '<h4 class="card-title">\n',
                 '<a href="#">' + products[i].name + '</a>\n',
               '</h4>\n',
-              '<h5>$'+ products[i].price +'</h5>\n',
+              '<h5>R$'+ products[i].price +'</h5>\n',
+              '<h5>Categoria: '+ products[i].category+'</h5>\n',
             '</div>\n',
             '<div class="card-footer">\n',
               '<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>\n',
